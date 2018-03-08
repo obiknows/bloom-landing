e0e46640b4f551f7deb4a73ea0d21aa0f16901a3
obi <obiknows88@gmail.com>
2018-03-08 11:05:39 -0800

    took out middle section

--- js/demo.js
+++ js/demo.js
@@ -10,9 +10,9 @@
  */
 {
 	setTimeout(() => document.body.classList.add('render'), 60);
-	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
-	const total = navdemos.length;
-	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+	// const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+	// const total = navdemos.length;
+	// const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
 	const navigate = (linkEl) => {
 		document.body.classList.remove('render');
 		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
